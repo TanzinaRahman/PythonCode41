1b64576ae2ba34149a8b0d8f9cbd9d34cdbf0656
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-30 20:51:12 +0600

    Create PythonProgram41.py

---
+++ PythonProgram41.py
@@ -0,0 +1,14 @@
+# Stack And Queue
+
+books = []
+books.append("Learn C")
+books.append("Learn C++")
+books.append("Learn Java")
+print(books)
+books.pop()
+print("Now the top book is : ", books[-1])
+books.pop()
+print("Now the top book is : ", books[-1])
+books.pop()
+if not books:
+    print("No books left")
