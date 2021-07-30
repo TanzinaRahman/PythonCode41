6ea684fb25fbb492d47f4df5cb04a37c13f21fdf
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-30 21:20:55 +0600

    Update PythonProgram41.py

--- PythonProgram41.py
+++ PythonProgram41.py
@@ -12,3 +12,16 @@ print("Now the top book is : ", books[-1])
 books.pop()
 if not books:
     print("No books left")
+     #Queue
+
+from collections import deque
+
+bank = deque(["Anis", "Karim", "Rahim"])
+print(bank)
+bank.popleft()
+print(bank)
+bank.popleft()
+bank.popleft()
+if not bank:
+    print("No person left")
+
